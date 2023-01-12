46041ba18d68ea28f051046772f49e6b9c63faee
gilbertosantana <gilbertosantoss307@gmail.com>
2023-01-12 20:10:22 -0300

    Foi adicionado os endpoints Get por Id, Post, Put e Delete

--- TarefasApi/Endpoints/TarefasEndPoints.cs
+++ TarefasApi/Endpoints/TarefasEndPoints.cs
@@ -10,7 +10,7 @@ namespace TarefasApi.Endpoints
         {
             app.MapGet("/", () => $"Bem-Vindo a API Tarefas {DateTime.Now}");
 
-            app.MapGet("/tarefas", async(GetConnection connectionGetter) =>
+            app.MapGet("/tarefas", async (GetConnection connectionGetter) =>
             {
                 using var con = await connectionGetter();
                 var tarefas = con.GetAll<Tarefa>().ToList();
@@ -19,6 +19,44 @@ namespace TarefasApi.Endpoints
                     return Results.NotFound();
                 }
                 return Results.Ok(tarefas);
+            });
+
+            app.MapGet("/tarefas/{id:int}", async (GetConnection conectionGetter, int id) =>
+            {
+                using var con = await conectionGetter();
+
+                return con.Get<Tarefa>(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound();
+            });
+
+            app.MapPost("/tarefas/", async (GetConnection connectionGetter, Tarefa tarefa) =>
+            {
+                using var con = await connectionGetter();
+                var id = con.Insert(tarefa);
+
+                return Results.Created($"/tarefas/{id}", tarefa);
+            });
+
+            app.MapPut("/tarefas/", async (GetConnection connectionGetter, Tarefa tarefa) =>
+            {
+                using var con = await connectionGetter();
+                var id = con.Update(tarefa);
+                
+                return Results.Ok();
+            });
+
+            app.MapDelete("/tarefas/{id:int}", async (GetConnection connectionGetter, int id) =>
+            {
+                using var con = await connectionGetter();
+
+                var deleted = con.Get<Tarefa>(id);
+
+                if(deleted is null)
+                {
+                    return Results.NotFound();
+                }
+                con.Delete(deleted);
+
+                return Results.Ok();
             });
         }
     }
